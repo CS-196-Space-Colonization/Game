b900ba4f8b740f27833b8ff81021403f08552cd8
aguerrero15 <aguerrero843@gmail.com>
2014-12-04 17:32:14 -0600

    Basic menu, update client

---
+++ Game/src/com/thecolony/tractus/Tractus.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.thecolony.tractus;
+
+/**
+ * Main game class. This is the class that we run to run the game.
+ * @author Arturo
+ */
+public class Tractus
+{
+    public static void main(String[] args)
+    {
+        new MainMenu();
+    }
+}
