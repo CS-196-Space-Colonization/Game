2389880e91913f751386cd569f5afc27d7a1fe0c
chthonic7 <fooliot@gmail.com>
2014-12-06 21:40:41 -0600

    Clean up, clean up. Everybody do your part...

--- Game/src/com/thecolony/tractus/worldgen/SpatialEntities/Cluster.java
+++ Game/src/com/thecolony/tractus/worldgen/SpatialEntities/Cluster.java
@@ -5,6 +5,7 @@ import com.thecolony.tractus.resources.Res;
 
 /**
  * Created by chthonic7 on 10/8/14.
+ *
  */
 public class Cluster extends RegionalEntity{
     public Cluster(Vector3f pos, Sector sector, StarSystem[] starsys, Res res, String name, String owner){
