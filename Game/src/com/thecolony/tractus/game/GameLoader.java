1c2e1592d79661d223f78b2efe55eef43fea6428
jbpagliuco <jpagli2@illinois.edu>
2014-12-10 16:28:51 -0600

    Revert "Revert "Fix last commit""
    This reverts commit 1555a0d3c7caccfe452f2f7d1d02a602339a8b95.

--- Game/src/com/thecolony/tractus/game/GameLoader.java
+++ Game/src/com/thecolony/tractus/game/GameLoader.java
@@ -95,7 +95,11 @@ public class GameLoader
         Filer filer=new Filer("tractus",!loadSave);
         TerrorGenerator.loadTerritories(loadSave,rootNode, planetsNode, mPlanets, starsNode, mSuns, assetManager, filer);
         loadSuckyEconomicSystem();
-        BattleGenerator.loadBattlers(loadSave,rootNode,loneShipsNode,loneShips,flotillasNode,flotillas,flotillaBattles,new double[5],filer);
+        double[] stats = new double[19];
+        loneShipsNode = new Node("Lone Ships");
+        flotillasNode = new Node("Flotillas Node");
+        stats[BattleObject.BATTLE_STAT_MOVEMENT_SPEED] = 5.0;
+        BattleGenerator.loadBattlers(loadSave,rootNode,loneShipsNode,loneShips,flotillasNode,flotillas,flotillaBattles,stats,filer);
         loadAmbientLight();
         loadSkybox();
         loadMovementPlane();
