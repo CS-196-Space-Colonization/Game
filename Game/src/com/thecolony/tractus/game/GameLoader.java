833045bbc07f200e4a7496539d4a60a92ea08b75
jbpagliuco <jpagli2@illinois.edu>
2014-12-10 17:10:53 -0600

    Revert "Something fixed."
    This reverts commit 575e1dade8c9777058d25ab05e17b561a4a78475.

--- Game/src/com/thecolony/tractus/game/GameLoader.java
+++ Game/src/com/thecolony/tractus/game/GameLoader.java
@@ -95,12 +95,9 @@ public class GameLoader
         Filer filer=new Filer("tractus",!loadSave);
         TerrorGenerator.loadTerritories(loadSave,rootNode, planetsNode, mPlanets, starsNode, mSuns, assetManager, filer);
         loadSuckyEconomicSystem();
-        loneShipsNode = new Node("Lone Ships");
-        flotillasNode = new Node("Flotillas Node");
-        double[] stats = new double[19];
-        stats[BattleObject.BATTLE_STAT_MOVEMENT_SPEED] = 5.0;
-        BattleGenerator.loadBattlers(loadSave,rootNode,loneShipsNode,loneShips,flotillasNode,flotillas,flotillaBattles,stats,filer);
+        BattleGenerator.loadBattlers(loadSave,rootNode,loneShipsNode,loneShips,flotillasNode,flotillas,flotillaBattles,new double[5],filer);
         loadAmbientLight();
+        loadShips();
         loadSkybox();
         loadMovementPlane();
         loadPictures();
